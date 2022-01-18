f4d05181dbe886d73316b2da76a2423a217d22fd
Adnan Karšić <adokce@gmail.com>
2022-01-18 15:28:55 +0100

    Form that dispatches the notification.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,39 @@
-import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
+  const [variant, setVariant] = useState("");
+  const [text, setText] = useState("");
+
+  const handleSubmit = () => {
+    console.log({ variant, text });
+    // triggerToast(variant,text)
+  };
+
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <label>
+        Select the type of notification:
+        <select value={variant} onChange={(e) => setVariant(e.target.value)}>
+          <option value="warning">warning</option>
+          <option value="success">success</option>
+          <option value="danger">danger</option>
+        </select>
+      </label>
+
+      <br />
+      <label>
+        Enter the notification text:
+        <textarea
+          value={text}
+          onChange={(e) => {
+            setText(e.target.value);
+          }}
+        />
+      </label>
+
+      <br />
+
+      <input type="submit" value="Submit" />
+    </form>
   );
 }
